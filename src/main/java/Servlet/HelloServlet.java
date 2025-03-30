5bf546973f00fb979916590331813dbea2166a4c
Firecx <nikandrov.alekseix1@gmail.com>
2025-03-30 19:32:04 +0600

    test

--- src/main/java/Servlet/HelloServlet.java
+++ src/main/java/Servlet/HelloServlet.java
@@ -1,4 +1,28 @@
 package Servlet;
 
-public class HelloServlet {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet({"/hello"})
+public class HelloServlet extends HttpServlet {
+    public HelloServlet() {
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter writer = response.getWriter();
+
+        try {
+            writer.println("<h2>Hello from HelloServlet</h2>");
+        } finally {
+            writer.close();
+        }
+
+    }
 }
