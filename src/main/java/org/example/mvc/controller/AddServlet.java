c414e71d28a00dd2f9a3a6c610329e457d037334
iaroslavchik <y.kovanev@mail.ru>
2025-04-06 17:27:31 +0600

    implement doGet for AddServlet

--- src/main/java/org/example/mvc/controller/AddServlet.java
+++ src/main/java/org/example/mvc/controller/AddServlet.java
@@ -1,24 +1,39 @@
 package org.example.mvc.controller;
 
-import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import org.example.mvc.model.Database;
 
-
-@WebServlet("/add")
 public class AddServlet extends HttpServlet {
-    private Database dataBase;
+    private Database database;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/add.jsp");
+        if (requestDispatcher == null) {
+            PrintWriter writer = response.getWriter();
+
+            try {
+                writer.println(this.getServletContext().getContextPath());
+            } finally {
+                writer.close();
+            }
+            return;
+        }
+        requestDispatcher.forward(request, response);
+    }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        this.dataBase = Database.getInstance();
-        dataBase.addStudent(request.getParameter("userid"),request.getParameter("username"));
+        this.database = Database.getInstance();
+        database.addStudent(request.getParameter("userid"), request.getParameter("username"));
     }
 }
-
-
