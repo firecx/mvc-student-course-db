924a2cdce65186d7954ef93ff9865858cf42ccbe
iaroslavchik <y.kovanev@mail.ru>
2025-04-09 19:19:32 +0600

    refactor WelcomeServlet

--- src/main/java/org/example/mvc/controller/WelcomeServlet.java
+++ src/main/java/org/example/mvc/controller/WelcomeServlet.java
@@ -1,51 +1,26 @@
-package org.example.mvc.controller;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+package org.example.mvc.controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("")
 public class WelcomeServlet extends HttpServlet {
-    public WelcomeServlet() {
-    }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
         response.setContentType("text/html");
-        PrintWriter writer = response.getWriter();
 
-        try {
+        try (PrintWriter writer = response.getWriter()) {
             writer.printf("<h1>Welcome from %s</h1>", this.getClass().getSimpleName());
-        } finally {
-            writer.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
-
     }
-
-//    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-//            throws ServletException, IOException {
-//
-//        response.setContentType("text/html");
-//        PrintWriter writer = response.getWriter();
-//
-//        String name = request.getParameter("username");
-//        String age = request.getParameter("userage");
-//        String gender = request.getParameter("gender");
-//        String country = request.getParameter("country");
-//        String[] courses = request.getParameterValues("courses");
-//
-//        try {
-//            writer.println("<p>Name: " + name + "</p>");
-//            writer.println("<p>Age: " + age + "</p>");
-//            writer.println("<p>Gender: " + gender + "</p>");
-//            writer.println("<p>Country: " + country + "</p>");
-//            writer.println("<h4>Course</h4>");
-//            for(String course: courses)
-//                writer.println("<li>" + course + "</li>");
-//        } finally {
-//            writer.close();
-//        }
-//    }
 }
