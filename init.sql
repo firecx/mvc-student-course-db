1a46cd562428f96d5c2a6fff619f8a168a8acac2
Firecx <nikandrov.alekseix1@gmail.com>
2025-07-24 15:03:45 +0600

    add courses table

--- init.sql
+++ init.sql
@@ -1,6 +1,14 @@
 CREATE TABLE IF NOT EXISTS students (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(50) UNIQUE NOT NULL,
-    email VARCHAR(100),
-    phone VARCHAR(100)
+    name TEXT UNIQUE NOT NULL,
+    email TEXT,
+    phone TEXT
+);
+
+CREATE TABLE IF NOT EXISTS courses (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    description TEXT NOT NULL,
+    duration TEXT NOT NULL,
+    price MONEY NOT NULL,
 );
