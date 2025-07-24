bcc4ebd926d9ffd1ed3d4aba1f46b45e5de2b4ad
Firecx <nikandrov.alekseix1@gmail.com>
2025-07-24 14:41:29 +0600

    expanded

--- src/main/java/org/example/mvc/model/units/Course.java
+++ src/main/java/org/example/mvc/model/units/Course.java
@@ -1,19 +1,44 @@
 package org.example.mvc.model.units;
 
 public class Course {
-    private String id;
+    private int id;
     private String name;
+    private String description;
+    private String duration;
+    private int price;
 
-    public Course(String id, String name) {
+    public Course(int id, String name, String description, String duration, int price) {
         this.id = id;
         this.name = name;
+        this.description = description;
+        this.duration = duration;
+        this.price = price;
     }
 
-    public String getId() {
+    public Course(String name, String description, String duration, int price) {
+        this.name = name;
+        this.description = description;
+        this.duration = duration;
+        this.price = price;
+    }
+
+    public int getId() {
         return id;
     }
 
     public String getName() {
         return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public int getPrice() {
+        return price;
     }
 }
