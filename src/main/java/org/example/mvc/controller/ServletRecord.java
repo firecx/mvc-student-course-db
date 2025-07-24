a157abd6de5847adf0c0ceb2c791a6a8c0b5703c
Firecx <nikandrov.alekseix1@gmail.com>
2025-07-24 16:25:23 +0600

    switching to postgresql

--- src/main/java/org/example/mvc/controller/ServletRecord.java
+++ src/main/java/org/example/mvc/controller/ServletRecord.java
@@ -3,7 +3,11 @@ package org.example.mvc.controller;
 import java.io.IOException;
 import java.sql.SQLException;
 
-import org.example.mvc.model.DataBaseSQLite;
+import org.example.mvc.model.DAO.CourseDAO;
+import org.example.mvc.model.DAO.RecordDAO;
+import org.example.mvc.model.DAO.StudentDAO;
+import org.example.mvc.model.units.Course;
+import org.example.mvc.model.units.Student;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -13,29 +17,56 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet("/add/record")
 public class ServletRecord extends HttpServlet {
-    private String returnPath = "/WEB-INF/pages/record.jsp";
-
-    private DataBaseSQLite dataBaseSQLite = DataBaseSQLite.getInstance();
+    private String returnPath = "/WEB-INF/pages/add/record.jsp";
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) {
         String courseName = request.getParameter("courseName");
         String studentName = request.getParameter("studentName");
-        String condition;
 
         if (courseName.isEmpty() || studentName.isEmpty()) {
             request.setAttribute("message", "Все поля должны быть заполнены!");
-        } else {
+        }
+        else {
             try {
-                condition = dataBaseSQLite.insertDataRecord(courseName, studentName);
-            } catch (SQLException e) {
+                Course course;
+                Student student;
+
+                try {
+                    course = new CourseDAO().getCourseByName(courseName);
+                } 
+                catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+
+                if (course == null){
+                    request.setAttribute("message", "Курс не найден!");
+                }
+                else {
+                    try {
+                        student = new StudentDAO().getStudentByName(studentName);
+                    }
+                    catch (SQLException e) {
+                        throw new RuntimeException(e);
+                    }
+
+                    if (student == null) {
+                        request.setAttribute("message", "Студент не найден!");
+                    }
+                    else {
+                        new RecordDAO().createRecord(course, student);
+                    } 
+                }
+                
+            }
+            catch (SQLException e) {
                 throw new RuntimeException(e);
             }
-            request.setAttribute("message", condition);
+            request.setAttribute("message", "Успешная запись!");
         }
         try {
             request.getRequestDispatcher(returnPath).forward(request, response);
-        } catch (ServletException e) {
+        }catch (ServletException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         } catch (IOException e) {
